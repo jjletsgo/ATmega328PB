7deebb83950284fc4c1f90e925159fee38012965
jjletsgo <dnrgus3914@knu.ac.kr>
2025-11-27 09:34:17 +0900

    d

---
+++ ATmega328PB_study/ATmega328PB_study/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATmega328PB_study.c
+ *
+ * Created: 2025-11-27 오전 9:29:32
+ * Author : User
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
